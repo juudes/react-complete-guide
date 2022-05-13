aa3d4086f73a74857c57b37a240c891c29b55544
judy <jduong9@gatech.edu>
2022-05-12 23:56:36 -0700

    44. An Alternative Function Syntax

--- src/components/SharedUI/Card.js
+++ src/components/SharedUI/Card.js
@@ -1,6 +1,6 @@
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
   const className = "card " + props.className;
   return <div className={className}>{props.children}</div>;
 }
